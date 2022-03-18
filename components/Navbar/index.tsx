5bb55feda064ebba96d4a45414db8d3a3c495808
james chege <jawiwy@gmail.com>
2022-03-18 22:52:07 +0400

    initial packages and modules wrap for interacting with the components

--- components/Navbar/index.tsx
+++ components/Navbar/index.tsx
@@ -1,10 +1,44 @@
-import React from 'react'
+import React, { useMemo } from 'react'
 import Link from 'next/link'
-import {Button, Avatar} from 'antd'
-import {PlusCircleOutlined, UserOutlined} from '@ant-design/icons'
+import { Button, Avatar } from 'antd'
+import { PlusCircleOutlined, UserOutlined } from '@ant-design/icons'
 import styles from './Navbar.module.css'
-
+import { useConnection, useWallet, WalletProvider } from '@solana/wallet-adapter-react'
+import {
+  LedgerWalletAdapter,
+  PhantomWalletAdapter,
+  SlopeWalletAdapter,
+  SolflareWalletAdapter,
+  SolletExtensionWalletAdapter,
+  SolletWalletAdapter,
+  TorusWalletAdapter,
+} from '@solana/wallet-adapter-wallets';
+import {
+  WalletModalProvider,
+  WalletDisconnectButton,
+  WalletMultiButton,
+  WalletConnectButton
+} from '@solana/wallet-adapter-react-ui';
+import { WalletAdapterNetwork } from '@solana/wallet-adapter-base'
 function Navbar() {
+  const connection = useConnection();
+  const wallet = useWallet();
+
+  const network = WalletAdapterNetwork.Devnet;
+
+  const wallets = useMemo(
+    () => [
+      new PhantomWalletAdapter(),
+      new SlopeWalletAdapter(),
+      new SolflareWalletAdapter({ network }),
+      new TorusWalletAdapter(),
+      new LedgerWalletAdapter(),
+      new SolletWalletAdapter({ network }),
+      new SolletExtensionWalletAdapter({ network }),
+    ],
+    [network]
+  );
+
   return (
     <header className={styles.container}>
       <div className={styles.right}>
@@ -18,6 +52,8 @@ function Navbar() {
         <div className={styles.create}>
           <Link href="/new/title">
             <a>
+
+
               <Button type="primary" shape="round" icon={<PlusCircleOutlined />} size="large">
                 Create
               </Button>
@@ -25,7 +61,18 @@ function Navbar() {
           </Link>
         </div>
         <div>
-          <Avatar size={48} icon={<UserOutlined />} />
+          {wallet.connected ?
+            <Avatar size={48} icon={<UserOutlined />} />
+            :
+            <>
+              <WalletProvider wallets={wallets} autoConnect>
+                <WalletModalProvider>
+                  <WalletMultiButton />
+                  {/* <WalletConnectButton /> */}
+                </WalletModalProvider>
+              </WalletProvider>
+            </>
+          }
         </div>
       </div>
     </header>
