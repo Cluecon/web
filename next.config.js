e5c9d129411ed1a90b18cc4e91f414e01b6c66d1
noickare <likono@clueconn.com>
2022-04-06 10:56:34 -0500

    added sentry

--- next.config.js
+++ next.config.js
@@ -2,8 +2,9 @@
 /** @type {import('next').NextConfig} */
 
 const withAntdLess = require('next-plugin-antd-less')
+const { withSentryConfig } = require('@sentry/nextjs')
 
-module.exports = withAntdLess({
+const moduleExports = withAntdLess({
   images: {
     domains: ['dev-ipfs.clueconn.com', 'ipfs.clueconn.com'],
   },
@@ -34,3 +35,17 @@ module.exports = withAntdLess({
     webpack5: true,
   },
 })
+
+const sentryWebpackPluginOptions = {
+  // Additional config options for the Sentry Webpack plugin. Keep in mind that
+  // the following options are set automatically, and overriding them is not
+  // recommended:
+  //   release, url, org, project, authToken, configFile, stripPrefix,
+  //   urlPrefix, include, ignore
+
+  silent: false, // Suppresses all logs
+  // For all available options, see:
+  // https://github.com/getsentry/sentry-webpack-plugin#options.
+}
+
+module.exports = withSentryConfig(moduleExports, sentryWebpackPluginOptions)
